f4bf9e3489d431af7b71fb2607c81dcc8ec597c0
AS0111 <145197001+AS0111@users.noreply.github.com>
2023-10-28 13:07:14 +0300

    Update commands.clj

--- functional_programming/mire/src/mire/commands.clj
+++ functional_programming/mire/src/mire/commands.clj
@@ -37,12 +37,8 @@
                             (:inhabitants @player/*current-room*)
                             (:inhabitants target))
          (ref-set player/*current-room* target)
-         (if (@player/*current-room* :hall)
-            (do 
-             
-             (player/game-logic config)))
+         (player/game-logic config)
          (look)
-         
        "You can't go that way."))))
 
 (defn grab
