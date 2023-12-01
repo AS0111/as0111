1969ddf169970f16cea8f96928b6876f12ae69e7
AS <145197001+AS0111@users.noreply.github.com>
2023-12-01 18:24:37 +0300

    Update player.clj

--- functional_programming/mire/src/mire/player.clj
+++ functional_programming/mire/src/mire/player.clj
@@ -81,7 +81,7 @@
     (if (or (<= (:hp player) 0)
             (<= (:hp enemy) 0))
       (print-winner (:hp player) (:hp enemy))
-        (let [update-in player [:att] #(+ % 240)] 
+        (let [(update-in (:att player) #(+ % 240))] 
           [pl->en (take-damage-win player enemy)
               en->pl (take-damage-win enemy player)]
           (do (print-attack-log (pl->en 0) (pl->en 1))
